2690dd354e8ff0d3ff06d6cd8d28c983e535a3e8
jaimelop83 <jaime.a.lopez@gmail.com>
2023-01-28 09:08:20 -0500

    Solved for object-syntax.js

--- 02-object-properties/object-syntax-practice/object-syntax.js
+++ 02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+
+const name = 'Cody';
+const loud = 'loud';
+
+function dog = {
+    break() {
+        return 'ruff ruff!';
+    },
+    name,
+    [loud + 'Bark']() {
+        return this.bark().toUpperCase();
+    },
+};
