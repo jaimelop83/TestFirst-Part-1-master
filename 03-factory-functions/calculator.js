68917deaf2116955545b30ebc588d0f7b80118ca
jaimelop83 <jaime.a.lopez@gmail.com>
2023-01-27 22:51:05 -0500

    Solved for factory functions - calculator partially

--- 03-factory-functions/calculator.js
+++ 03-factory-functions/calculator.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+
+const createCalculator = () => ({
+  total: 0,
+  add(num) {
+    this.total += num;
+  },
+  substract(num) {
+    this.total -= num;
+  },
+  clear() {
+    this.total = 0;
+  },
+});
