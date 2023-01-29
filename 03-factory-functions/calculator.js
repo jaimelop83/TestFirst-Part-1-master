5336176e6e6004836f73baa6c9bc4b1f9ef1fb11
jaimelop83 <jaime.a.lopez@gmail.com>
2023-01-29 16:02:53 -0500

    Solved for factory functions - calculator

--- 03-factory-functions/calculator.js
+++ 03-factory-functions/calculator.js
@@ -5,10 +5,51 @@ const createCalculator = () => ({
   add(num) {
     this.total += num;
   },
-  substract(num) {
+  subtract(num) {
     this.total -= num;
   },
   clear() {
     this.total = 0;
   },
+  value() {
+    return this.total;
+  },
+  clear() {
+    this.total = 0;
+  },
 });
+
+function addSquareMethod(arrOfCalc) {
+  arrOfCalc.forEach((calc) => {
+    calc.square = function () {
+      return this.total * this.total;
+    };
+  });
+
+  return arrOfCalc;
+}
+
+const prototype = {
+  total: 0,
+  add(num) {
+    this.total += num;
+  },
+  subtract(num) {
+    this.total -= num;
+  },
+  clear() {
+    this.total = 0;
+  },
+  value() {
+    return this.total;
+  },
+  clear() {
+    this.total = -10;
+  },
+};
+
+function createHumanCalculator() {
+  let instance = Object.create(prototype);
+  instance.total = -10;
+  return instance;
+}
